eef6de00942fd67e671ad65eab7c57cd4ef4714a
plastik <gabor@antavo.com>
2018-10-15 15:21:16 +0200

    Added unit tests for RestClient __construct() & getCredentialScope().

--- tests/RestClientTest.php
+++ tests/RestClientTest.php
@@ -13,4 +13,25 @@ class RestClientTest extends PHPUnit\Framework\TestCase {
             new \Antavo\Sdk\RestClient('st2', 'key', 'secret')
         );
     }
+
+    /**
+     * @covers \Antavo\Sdk\RestClient::__construct()
+     */
+    public function testConstructor() {
+        $this->assertSame(
+            'https://api.st2.antavo.com',
+            (new Antavo\Sdk\RestClient('st2', 'key', 'secret'))->getBaseUrl()
+        );
+    }
+
+    /**
+     * @covers \Antavo\Sdk\RestClient::getCredentialScope()
+     */
+    public function testGetCredentialScope() {
+        $this->assertSame(
+            'st2/api/antavo_request',
+            (new Antavo\Sdk\RestClient('st2', 'key', 'secret'))
+                ->getCredentialScope()
+        );
+    }
 }
